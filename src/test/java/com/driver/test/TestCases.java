103ea2fbac854736bc6e55dd5651d5152047e272
PRASHANT PAREKH <prashantparekh1501@gmail.com>
2025-10-29 17:10:19 +0530

    overriding done

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,11 +2,5 @@ package com.driver.test;
 
 
 public class TestCases {
-   public static void main(String[]args){
-       A a = new A();
-       System.out.println(a.meth());
 
-       B b = new B();
-       System.out.println(b.meth());
-   }
 }
---
+++ src/test/java/com/driver/test/main.java
@@ -0,0 +1,11 @@
+package com.driver.test;
+
+import javax.swing.plaf.synth.SynthStyle;
+
+public class main {
+    public static void main(String[]args){
+        B obj = new B();
+        System.out.println(obj.meth());
+    }
+
+}
